178d3bd96e2547b751d78ea26bb501ddb7e70a70
feildrixliemdra <feildrix.liemdra@efishery.com>
2024-10-15 17:12:37 +0700

    feat: implement mobile navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import Image from 'next/image'
+import Sidebar from '../../components/Sidebar'
+import MobileNavbar from '../../components/Mobile/MobileNavbar'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const loggedInUser = { firstName: 'Babaev', lastName: 'Vana' }
   return (
-    <main>
-      sidebar
-      {children}
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar user={loggedInUser} />
+
+      <div className='flex size-full flex-col'>
+        <div className='root-layout'>
+          <Image src='/icons/logo.svg' width={30} height={30} alt='menu' />
+          <div>
+            <MobileNavbar user={loggedInUser} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   )
 }
---
+++ components/Mobile/MobileNavbar.tsx
@@ -0,0 +1,84 @@
+'use client'
+import React from 'react'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { AlignJustify } from 'lucide-react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { sidebarLinks } from '../../constants'
+import { usePathname } from 'next/navigation'
+import { cn } from '../../lib/utils'
+
+const MobileNavbar = ({ user }: MobileNavProps) => {
+  const pathName = usePathname()
+
+  return (
+    <section id='mobile-nav' className='w-full max-w-[264px]'>
+      <Sheet>
+        <SheetTrigger>
+          <AlignJustify size={30} />
+        </SheetTrigger>
+        <SheetContent side='left' className='border-none bg-white'>
+          <Link
+            className='cursor-pointer flex items-center gap-1 px-4'
+            href='/'
+          >
+            <Image src='/icons/logo.svg' width={34} height={34} alt='Logo' />
+            <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>
+              Horizon
+            </h1>
+          </Link>
+          <div className='mobilenav-sheet'>
+            <SheetClose asChild>
+              <nav className='flex h-full flex-col gap-6 pt-16 text-white'>
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathName === item.route ||
+                    pathName.startsWith(`${item.route}/`)
+                  return (
+                    <SheetClose asChild key={item.label}>
+                      <Link
+                        className={cn('mobilenav-sheet_close w-full', {
+                          'bg-bankGradient': isActive,
+                        })}
+                        href={item.route}
+                        key={item.label}
+                      >
+                        <div className='size-6 relative'>
+                          <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                            className={cn({
+                              'brightness-[3] invert-0': isActive,
+                            })}
+                          />
+                        </div>
+                        <p
+                          className={cn('text-16 font-semibold text-black-2', {
+                            '!text-white': isActive,
+                          })}
+                        >
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  )
+                })}
+                User
+              </nav>
+            </SheetClose>
+            footer
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  )
+}
+
+export default MobileNavbar
