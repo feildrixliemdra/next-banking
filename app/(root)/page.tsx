03b6cd4abf51ac6cc8e39b527737b45c71320fc6
feildrixliemdra <feildrix.liemdra@efishery.com>
2024-10-15 19:17:45 +0700

    feat: implement sign in page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import HeaderBox from '../../components/HeaderBox'
 import TotalBalanceBox from '../../components/TotalBalanceBox'
+import RightSidebar from '../../components/RightSidebar'
 
 const Home = () => {
-  const user = { firstName: 'Babaev' }
+  const user = {
+    firstName: 'Babaev',
+    lastName: 'Vana',
+    email: 'arthurbbv05@gmail.com',
+  }
   return (
     <section id='home' className='home'>
       <div className='home-content'>
-        <HeaderBox
-          type='greeting'
-          title='Welcome'
-          subtext='Manage your bank account and transaction effeciently'
-          user={user?.firstName || 'Guest'}
-        />
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={2}
-          totalCurrentBalance={33232.24}
-        />
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            subtext='Manage your bank account and transaction effeciently'
+            user={user?.firstName || 'Guest'}
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={2}
+            totalCurrentBalance={33232.24}
+          />
+        </header>
+        Recent Transaction Table
       </div>
+      <RightSidebar
+        user={user}
+        banks={[
+          { currentBalance: 123.5, mask: '1123' },
+          { currentBalance: 500.5, mask: '3453' },
+        ]}
+      />
     </section>
   )
 }
---
+++ form-schema/AuthFormSchema.tsx
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+
+export const authFormSchema = (type: string) =>
+  z.object({
+    // sign up
+    firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+    city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+    state:
+      type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
+    postalCode:
+      type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+    dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    // both
+    email: z.string().email({ message: 'must contain valid email format' }),
+    password: z
+      .string()
+      .min(8, { message: 'must contain at least 8 character' }),
+  })
